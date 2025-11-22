0989bac9d6f50043457cec9acc5ec33544ab6e18
Ian-926 <139288819+Ian-926@users.noreply.github.com>
2025-11-22 12:42:38 -0500

    Add files via upload
    made it look nice

--- HackJam.py
+++ HackJam.py
@@ -1,27 +1,43 @@
 import customtkinter
-import time
+import PIL
 
 app = customtkinter.CTk()
 
+app.title("Login Screen")
 
-app.title("test")
-app.attributes('-fullscreen', True)
-app.geometry("500x500")
+screen_width = app.winfo_screenwidth()
+screen_height = app.winfo_screenheight()
+
+customtkinter.set_appearance_mode("Light")
+
+image = PIL.Image.open("Wallpaper.jpg")
+backgroundImage = customtkinter.CTkImage(image, size=(screen_width, screen_height))
+bgLabel = customtkinter.CTkLabel(app, image=backgroundImage, text="")
+bgLabel.place(x=0, y=0, relwidth=1, relheight=1)
+
+app.geometry(f"{screen_width}x{screen_height}+0+0")
 
 def submit():
     # Retrieve the text from the entry box
-    user_input = my_entry.get()
+    user_input = username.get()
     print(f"User entered: {user_input}")
 
-my_entry = customtkinter.CTkEntry(app, placeholder_text="Enter your text here")
-my_entry.pack(pady=20, padx=20)
+#blank text for padding
+padding = customtkinter.CTkLabel(app, text="")
+padding.pack(pady = 70, padx = 20)
 
-my_entry2 = customtkinter.CTkEntry(app, placeholder_text="Enter your text here")
-my_entry2.pack(pady=20, padx=20)
+welcome = customtkinter.CTkLabel(app, text="Welcome back, User", font=("Calibri", 50, "bold"), fg_color="#D7E9F3")
+welcome.pack(pady=20, padx = 20)
+
+username = customtkinter.CTkEntry(app, placeholder_text="Username", width=300, height=50, font=("Calibri", 20, "bold"))
+username.pack(pady=20, padx=20)
+
+password = customtkinter.CTkEntry(app, placeholder_text="Password", width=300, height=50, font=("Calibri", 20, "bold"))
+password.pack(pady=20, padx=20)
 
 # Create a button to process the input
-my_button = customtkinter.CTkButton(app, text="Submit", command=submit)
-my_button.pack(pady=10)
+submitButton = customtkinter.CTkButton(app, text="Submit", command=submit, width=150, height = 50, font=("Calibri", 20, "bold"))
+submitButton.pack(pady=10)
 
 app.mainloop()
 
